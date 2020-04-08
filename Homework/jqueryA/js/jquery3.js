30abfbe2ca43849baeaab7a09b9ce35443a3c031
Wils Taylor <wils@Wilss-MBP.attlocal.net>
2020-04-07 21:47:53 -0400

    b

---
+++ Homework/jqueryA/js/jquery3.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+$("footer").mouseover(function(){
+  $("footer th").html("NEXT STOP:");
+});
+
+$("footer").mouseover(function(){
+  $("footer td").html("TOKYO!");
+});
+
+$("footer").mouseover(function(){
+  $("footer td3").html("HOKKAIDO!");
+});
+
+$("main").mouseover(function(){
+  $("main p1").html("Make sure to visit Shibuya and Harajuku for amazing nightlife and food. Ramen shops are on every corner throughout North Village in Shibuya!")
+});
+
+
+
+});
