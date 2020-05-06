b0f5718da57c90b94835312c926fa5ba5191bf21
Wils Taylor <wils@Wilss-MBP.attlocal.net>
2020-05-05 23:15:10 -0400

    h

---
+++ Homework/responsivesite/js/flow.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+$("footer").mouseover(function(){
+  $("footer th").html("COMING IN 2020!!");
+});
+
+$("h2").mouseover(function(){
+  $("h2").html("STREET-WEARABLE ART");
+});
+
+});
